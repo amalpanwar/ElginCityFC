036de09e790208122dd4eae4396e6c39a365bb45
Amal Panwar <panwar.amal1995@gmail.com>
2025-02-12 20:53:51 +0000

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -32,7 +32,7 @@ from langchain_community.embeddings import HuggingFaceHubEmbeddings
 # from langchain_mistralai import ChatMistralAI
 # from langchain_together import ChatTogether
 from langchain_llm import ChatAI21
-from langchain_deepseek import ChatDeepSeek
+# from langchain_deepseek import ChatDeepSeek
 #from unstructured.partition.xlsx import partition_xlsx
 import networkx as nx
 #import pandas as pd
