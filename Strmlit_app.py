0243fb114b70e10b9a20138c6e2e6c977319072e
Amal Panwar <panwar.amal1995@gmail.com>
2025-05-17 01:57:02 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -1584,7 +1584,7 @@ elif position == 'CF':
     df_filtered2['Overall Threat Expectation'] = df_filtered2['xG per 90'] + df_filtered2['xA per 90']
 
 # Sorting the DataFrame by 'Goals + Assists per 90', 'Goals per 90', and 'Assists per 90' in descending order
-    df_filtered3 = df_filtered2.sort_values(by=['Overall Threat built'], ascending=False)
+    df_filtered3 = df_filtered2.sort_values(by=['Overall Threat Expectation'], ascending=False)
 
     # Melt the dataframe to long format for stacking
     df_melted = df_filtered3.melt(id_vars='Player', value_vars=['xG per 90','xA per 90'], var_name='Metric', value_name='Value')
